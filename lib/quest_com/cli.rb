0673d67ff37c3fb922f6827a250379faef164691
Meredith Reece <nerdybug@gmail.com>
2017-08-06 04:20:39 +0000

    flesh out CLI and start Scraper

--- lib/quest_com/cli.rb
+++ lib/quest_com/cli.rb
@@ -1,4 +1,24 @@
 class QuestCom::CLI
 
+  def call
+    greet
+    input = gets.strip
+    get_quest_name(input)
+    scrape_for_id(input)
+    # not yet gotten to collecting comments, displaying them or other options
+  end
+
+  def greet
+    puts "Type in the exact quest name then hit ENTER to see its top comment from wowhead:"
+  end
+
+  def get_quest_name(user_input)
+    quest_name = user_input
+    puts "#{quest_name}" # for testing while building at the moment
+  end
+
+  def scrape_for_id(user_input)
+    QuestCom::Scraper.new(user_input)
+  end
 
 end
--- lib/quest_com/scraper.rb
+++ lib/quest_com/scraper.rb
@@ -1,4 +1,8 @@
 class QuestCom::Scraper
 
+  def initialize(user_input)
+    # save this as a variable that can be put out with the results: You searched for "#{user_input}"...
+    @user_input = user_input
+  end
 
 end
