9fc7222c8bd76198bb9138906872e59b37ee6437
Meredith Reece <nerdybug@gmail.com>
2017-08-09 05:22:05 +0000

    begin creation of QuestData object

--- lib/quest_com/quest_data.rb
+++ lib/quest_com/quest_data.rb
@@ -1,16 +1,15 @@
 class QuestCom::QuestData
   attr_reader :quest_id
+
   def initialize(quest_id, page_data)
-    # stuff
     # takes in a scraped quest's information (result_body, quest_id)
     @quest_id = quest_id
     @data = page_data
-    puts "Hello, I am the Quest! My ID is #{@quest_id}."
+    puts "Hello, I am the Quest! My ID is #{@quest_id}." # only here for testing at the moment
     # binding.pry
   end
 
   def find_all_comments
-    # stuff
     # finds all of the comments...ALL OF THEM
     # example usage once those are stored as a reader variable @comments
       # quest_match = QuestCom::QuestData.new(quest_id)
@@ -18,7 +17,7 @@ class QuestCom::QuestData
   end
 
   def find_top_comment
-    # stuff
     # has the top comment and any information about that comment (comments on it, time, date, author)
   end
+
 end
