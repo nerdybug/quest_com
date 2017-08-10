e86ae106f362a665f5ca431f8d441fa7ee602599
Meredith Reece <nerdybug@gmail.com>
2017-08-10 04:51:41 +0000

    extract html data for each comment

--- lib/quest_com/scraper.rb
+++ lib/quest_com/scraper.rb
@@ -1,3 +1,5 @@
+# require 'open-uri'
+# require 'nokogiri'
 class QuestCom::Scraper
 
   def initialize(user_input)
@@ -27,28 +29,37 @@ class QuestCom::Scraper
 
     if potential_matches.length == 1
       find_my_id = names.index("#{potential_matches[0]}").to_i
-      puts "The index for the quest is #{find_my_id}" # only here for testing at the moment
+      # puts "The index for the quest is #{find_my_id}" # only here for testing at the moment
       quest_id = parsedArray[7][find_my_id][1]
-      puts "The quest ID is #{quest_id}" # only here for testing at the moment
+      # puts "The quest ID is #{quest_id}" # only here for testing at the moment
       quest_id
     else
       puts "There is no match. Please try your query on wowhead.com" # temporary message
     end
   end
 
-  def find_data_from_quest_page(quest_id)
-    url = URI.parse("http://www.wowhead.com/q=#{quest_id}/")
-    request = Net::HTTP::Get.new(url.to_s)
-    result = Net::HTTP.start(url.host, url.port) {|http| http.request(request)}
-    result.body
+  def find_comments_on_quest_page(quest_id) # using Nokogiri
+    html_comments = []
+    url = "http://www.wowhead.com/quest=#{quest_id}/"
+    doc = Nokogiri::HTML(open(url))
+    comments_section = doc.search("div#user-comments").children
+
+    comments_section.each do |element|
+      if element.attr("class") == "user-post-"
+        html_comments << element.inner_html
+      end
+    end
+    html_comments # currently houses raw html data for each comment - need to clean up
+    binding.pry
   end
 
   def scrape_to_create_quest_object
     # do the work to get from the user's input to creating a QuestData object
     prepared_input = prepare_input_for_search(@user_input)
     quest_id = parse_quest_id(search_for_result_body(prepared_input))
-    page_data = find_data_from_quest_page(quest_id)
-    QuestCom::QuestData.new(quest_id, page_data)
+    sleep 5
+    comments_data = find_comments_on_quest_page(quest_id)
+    QuestCom::QuestData.new(quest_id, comments_data)
   end
 
 end
