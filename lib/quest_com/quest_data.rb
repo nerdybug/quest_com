7f598205e5eef0c3d22d491b1f8348e47f6a7a9b
Meredith Reece <nerdybug@gmail.com>
2017-08-19 20:31:07 +0000

    begin display of top comment

--- lib/quest_com/quest_data.rb
+++ lib/quest_com/quest_data.rb
@@ -1,23 +1,24 @@
 class QuestCom::QuestData
   attr_reader :quest_id
 
-  def initialize(quest_id, comments_data)
+  def initialize(quest_id, comments)
     # takes in a scraped quest's information (result_body, quest_id)
     @quest_id = quest_id
-    @comments_data = comments_data
-    puts "Hello, I am the Quest! My ID is #{@quest_id}." # only here for testing at the moment
+    @comment_objects = comments # array of comment objects made with OpenStruct
+    puts "Hello, I am the Quest! My ID is #{@quest_id}. I have #{@comment_objects.length} comments." # only here for testing at the moment
     # binding.pry
   end
 
-  def find_all_comments
-    # finds all of the comments...ALL OF THEM
-    # example usage once those are stored as a reader variable @comments
-      # quest_match = QuestCom::QuestData.new(quest_id)
-      # quest_match.all_comments => puts out every comment on that quest
+  def all_comments
+    # temporary
+
   end
 
-  def find_top_comment
+  def top_comment
     # has the top comment and any information about that comment (comments on it, time, date, author)
+    puts "#{@comment_objects[0]["body"]}" # should puts out the body of the first/top comment
+    # formatting handles line breaks but puts out [npc=number] for links to wowhead npc pages
+    # and also [url] for links and [b] for map coords
   end
 
 end
