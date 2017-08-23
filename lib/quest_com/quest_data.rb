0f361f41491f20499f45834e7552a0a73fd2d00c
Meredith Reece <nerdybug@gmail.com>
2017-08-23 03:41:58 +0000

    remove openstruct

--- lib/quest_com/quest_data.rb
+++ lib/quest_com/quest_data.rb
@@ -1,11 +1,10 @@
 class QuestCom::QuestData
   attr_reader :quest_id
 
-  def initialize(quest_id, comments)
-    # takes in a scraped quest's information (result_body, quest_id)
+  def initialize(quest_id, comment_hash_array)
     @quest_id = quest_id
-    @comment_objects = comments # array of comment objects made with OpenStruct
-    puts "Hello, I am the Quest! My ID is #{@quest_id}. I have #{@comment_objects.length} comments." # only here for testing at the moment
+    @comment_data = comment_hash_array # array of comment objects made with OpenStruct
+    puts "Hello, I am the Quest! My ID is #{@quest_id}. I have #{@comment_data.length} comments." # only here for testing at the moment
     # binding.pry
   end
 
@@ -16,7 +15,7 @@ class QuestCom::QuestData
 
   def top_comment
     # has the top comment and any information about that comment (comments on it, time, date, author)
-    puts "#{@comment_objects[0]["body"]}" # should puts out the body of the first/top comment
+    puts "#{@comment_data[0]["body"]}" # should puts out the body of the first/top comment
     # formatting handles line breaks but puts out [npc=number] for links to wowhead npc pages
     # and also [url] for links and [b] for map coords
   end
