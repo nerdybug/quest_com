ea61bf69b6eef18a4ed344a417bed84dad2e4d32
Meredith Reece <nerdybug@gmail.com>
2017-08-24 01:23:42 +0000

    require Comment file

--- lib/quest_com.rb
+++ lib/quest_com.rb
@@ -2,6 +2,7 @@ require './lib/quest_com/version'
 require './lib/quest_com/cli'
 require './lib/quest_com/scraper'
 require './lib/quest_com/quest_data'
+require './lib/quest_com/comment'
 
 require 'pry'
 require 'net/http'
