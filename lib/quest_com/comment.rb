201b2e00883548f8c77223242f7b1c2d303ab031
Meredith Reece <nerdybug@gmail.com>
2017-08-25 05:59:13 +0000

    current and top_comment indicators

--- lib/quest_com/comment.rb
+++ lib/quest_com/comment.rb
@@ -1,7 +1,9 @@
 class QuestCom::Comment
-  attr_accessor :commentv2, :number, :id, :nreplies, :sticky, :user, :body, :date,
+  attr_accessor :current, :top_comment, :commentv2, :number, :id, :nreplies, :sticky, :user, :body, :date,
   :rating, :indent, :roles, :deleted, :outofdate, :userRating, :replies, :lastEdit
-# of particular importance are: number, nreplies, user, body, date, rating
+# of particular importance are: current, top_comment, number, nreplies, user, body, date, rating
+# current is the Comment object currently being viewed
+# top_comment will be true or false
 # number is the numeral for its position on the page starting at 0
 # nreplies is how many replies the comment has
 # user is the wowhead user's name who posted the comment
@@ -9,9 +11,11 @@ class QuestCom::Comment
 # date is the day when the comment was posted, gsub with regex will remove the timestamp
 # rating is the actual rate for the comment as wowhead.com automatically sorts the display under highest rated
 
+
   def initialize(hash)
     hash.each {|key, value| send("#{key}=", value)}
     fix_date
+    top?
   end
 
   def fix_date
@@ -20,11 +24,32 @@ class QuestCom::Comment
   end
 
   def more_information
-    # neatly display additional details about a comment
-    # <<-HEREDOC
-    puts "Date posted: #{self.date}"
-    puts "Comment author: #{self.user}"
-    puts "Rating: #{self.rating}"
-    # HEREDOC
+    puts <<-HEREDOC
+    Date posted: #{self.date}
+    Comment author: #{self.user}
+    Rating: #{self.rating}
+  	HEREDOC
   end
+
+  # def comment_menu
+  #   puts <<-HEREDOC
+  #   1. see more information about this comment
+  #   2. see a list of other comments
+  #   3. search for another quest's top comment
+  #   4. exit
+  # 	HEREDOC
+  # end
+
+  def top?
+    # if a comment is the top comment, it will also me the current comment
+    if self.number == 0
+      self.top_comment = true
+      self.current = self
+    else
+      self.top_comment = false
+    end
+  end
+
+
+
 end
