c559a92a1a18820e907f4d5558ab0d3963f54b52
Meredith Reece <nerdybug@gmail.com>
2017-08-25 05:59:54 +0000

    menu construction

--- lib/quest_com/quest_data.rb
+++ lib/quest_com/quest_data.rb
@@ -1,5 +1,5 @@
 class QuestCom::QuestData
-  attr_reader :quest_id, :top_comment, :all_comments
+  attr_reader :quest_id, :all_comments
 
   def initialize(quest_id, comment_hash_array)
     @quest_id = quest_id
@@ -9,67 +9,92 @@ class QuestCom::QuestData
     # binding.pry
   end
 
-  # def all_comments
-  #   @comments # array of Comment objects
-  # end
-
   def create_and_store_comments(array_of_hashes)
     @all_comments = []
     array_of_hashes.each do |hash|
       @all_comments << QuestCom::Comment.new(hash)
     end
-    @top_comment = @all_comments[0]
     @all_comments
     # binding.pry
   end
 
-  # def top_comment
-  #   # has the top comment and any information about that comment (comments on it, time, date, author)
-  #   puts "#{@all_comments[0].body}" # should puts out the body of the first/top comment
-  #   # formatting handles line breaks but puts out [npc=number] for links to wowhead npc pages
-  #   # and also [url] for links and [b] for map coords
-  # end
-
   def reset
     @quest_id = nil
-    @top_comment = nil
     @all_comments = []
   end
 
   def other_comments
     puts "This will be a numbered list of the quest's other comments."
+    menu(["C", "N", "E"])
+    # example
+    # 2. "I am the first part of..." by User on 2013-08-02 - rating: 1
+    # 3. "This is the second part of..." by User2 on 2013-09-04 - rating 1
+    # options: pick number to see full comment, new search, exit; C, N, E
+  end
+
+  def get_all_comments
+    self.all_comments
   end
 
   def show_top_comment
-    top = top_comment
-    puts "#{top.body}"
+    comments = get_all_comments
+    comments.select do |comment|
+      if comment.top_comment
+        puts "#{comment.body}" # needs format tweaking still
+      end
+    end
   end
 
-  def options
-  	puts "This quest has #{all_comments.length - 1} more comments. Please select an option from the following:"
-    puts <<-HEREDOC
-    1. see more information about this comment
-    2. see a list of other comments
-    3. search for another quest's top comment
-    4. exit
-  	HEREDOC
-  	input = gets.strip
+  def find_current_comment
+    comments = get_all_comments
+    comments.select {|comment| comment.current == comment}
+  end
 
-  	if input == "1"
-  		top = top_comment
-  		top.more_information
-      # if this is selected, the information shows but the program fully exits
-      # need something like #comment_options (just list of other comments, search again or exit)
-  	elsif input == "2"
-  		other_comments
-    elsif input == "3"
+  def initial_menu
+    menu(["I", "L", "N", "E"])
+    input = gets.strip
+    analyze_input(input)
+  end
+
+  def menu(options)
+    # take in an array of letters representing options to give the user
+    puts "\nPlease select from the following:"
+    options.each do |opt|
+      case opt
+      when "I"
+        puts "I = see more Information about this comment"
+      when "L"
+        puts "L = see a List of other comments"
+      when "N"
+        puts "N = search for a New quest's top comment"
+      when "C"
+        puts "Choose the number of any listed comment to see its full text"
+      when "E"
+        puts "E = Exit"
+      end
+    end
+    input = gets.strip
+    analyze_input(input)
+  end
+
+  def analyze_input(input)
+    case input.downcase
+    when "i"
+      current = find_current_comment
+      current[0].more_information
+      menu(["L", "N", "E"])
+    when "l"
+      other_comments
+    when "n"
       reset
-  	elsif input == "4"
-  		exit
-  	else
-  		puts "That is not a valid response."
-  		options
-  	end
+      QuestCom::CLI.new.call
+    when "e"
+      exit
+    else
+      puts "Invalid selection."
+      initial_menu
+    end
+    # add in way to handle numeral entered from list of comments...
   end
 
 end
