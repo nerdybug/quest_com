f5de9de19849ab4dba7b72220a3e4d6872e6d696
Meredith Reece <nerdybug@gmail.com>
2017-08-27 02:49:01 +0000

    add sleep, change comments list

--- lib/quest_com/quest_data.rb
+++ lib/quest_com/quest_data.rb
@@ -4,7 +4,7 @@ class QuestCom::QuestData
   def initialize(quest_id, comment_hash_array)
     @quest_id = quest_id
     @comment_data = comment_hash_array # array of comment hash data
-    puts "Hello, I am the Quest! My ID is #{@quest_id}. I have #{@comment_data.length} comments." # only here for testing at the moment
+    puts "\nThe top comment for this quest is:\n\n" # only here for testing at the moment
     create_and_store_comments(@comment_data)
     # binding.pry
   end
@@ -24,18 +24,17 @@ class QuestCom::QuestData
   end
 
   def all_comments_list
-    puts "This will be a numbered list of all of the quest's comments." # here for testing
     comments = get_all_comments
     counter = 0
     comments.collect do |comment|
       body = comment.body
-      result = body.gsub(/\[url=.+\[\/url\]/, "") # this needs to be somewhere that tidies the comment body
-      new_result = result.split(/\s+/, 9)[0...8].join(' ') # pulls out the first handful of words
+      snippet = body.split(/\s+/, 9)[0...8].join(' ') # pulls out the first handful of words
       counter += 1
-      puts "#{counter}. #{new_result.strip}... #{comment.more_information}"
+      puts "#{counter}. #{snippet.strip}... #{comment.more_information}"
       # binding.pry
     end
     # options: pick number to see full comment, new search, exit; C, N, E
+    sleep 2
     menu(["N", "E", "C"])
   end
 
@@ -47,7 +46,7 @@ class QuestCom::QuestData
     comments = get_all_comments
     comments.select do |comment|
       if comment.top_comment
-        puts "#{comment.body}" # needs format tweaking still
+        puts "\"#{comment.body}\"" # needs format tweaking still
       end
     end
   end
@@ -58,6 +57,7 @@ class QuestCom::QuestData
   end
 
   def initial_menu
+    sleep 1
     menu(["I", "L", "N", "E"])
     input = gets.strip
     analyze_input(input)
@@ -75,7 +75,7 @@ class QuestCom::QuestData
       when "N"
         puts "N = search for a New quest's top comment"
       when "C"
-        puts "Or enter the number of any comment from the list to see its full text"
+        puts "Or enter the number of any comment from the numbered list to see its full text"
       when "E"
         puts "E = Exit"
       end
@@ -89,16 +89,19 @@ class QuestCom::QuestData
     when "i"
       current = find_current_comment
       puts "#{current[0].more_information}"
+      sleep 1
       menu(["L", "N", "E"])
     when "l"
       all_comments_list
     when "n"
       reset
+      sleep 1
       QuestCom::CLI.new.call
     when "e"
       exit
     else
       puts "Invalid selection."
+      sleep 1
       initial_menu
     end
     # add in way to handle numeral entered from list of comments...
