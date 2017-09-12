052f8a934efad687e5a6f74052462bf5a9d75f33
Meredith Reece <nerdybug@gmail.com>
2017-09-12 04:00:32 +0000

    use handler for scraped data

--- lib/quest_com/handler.rb
+++ lib/quest_com/handler.rb
@@ -3,4 +3,39 @@ class QuestCom::Handler
   def self.greet_user
     puts "Type in the exact quest name then hit ENTER to see its top comment from wowhead:"
   end
+
+  def self.prepare_input(input)
+    remove_regex = /\b(and|of|the|with)\b|[!?.,-_=;:&\(\)\[\]]/
+    result = input.downcase
+    result.gsub!(remove_regex, '')
+    result.squeeze(" ")
+  end
+
+  def self.analyze_matches(parsedArray, names, potential_matches)
+    if potential_matches.length == 1
+      find_my_id = names.index("#{potential_matches[0]}").to_i
+      quest_id = parsedArray[7][find_my_id][1]
+      quest_id
+    else
+      puts "There is no match. Please try your query on wowhead.com" # temporary message
+      # flesh this out a bit - specific text for no match vs too many matches?
+      # what if the quest HAS NO COMMENTS as well
+    end
+  end
+
+  def self.tidy_for_json(javascript)
+    # remove key/values that do not matter for the purpose of this program
+    javascript.gsub!(/\b(replies|lastEdit):(\[.*?\])/, '')
+    javascript.gsub!(/(\bcommentv2|number|id|nreplies|sticky|indent|roles|deleted|outofdate):.*?,/, '')
+    javascript.gsub!(/\b,userRating:\d+,+/, '')
+
+    # locate unescaped double quotes to properly escape
+    javascript.gsub!(/(?<!\\)(?:\\{2})*\K"/, '\"')
+    # the user, body and date values are surrounded with single quotes that need to be double
+    user_body_date_regex = /\b(?<key>user|body|date):(?<startQuote>')(?<value>(?:[^']|(?<=\\)')+)(?<endQuote>')/
+    javascript.gsub!(user_body_date_regex, '\k<key>:"\k<value>"')
+    # find each key and surround it with double quotes to make json parser happy
+    javascript.gsub!(/(?<=[{,])([\w]+):/, '"\1":')
+    javascript
+  end
 end
--- lib/quest_com/scraper.rb
+++ lib/quest_com/scraper.rb
@@ -5,10 +5,7 @@ class QuestCom::Scraper
   end
 
   def prepare_input_for_search(input)
-    remove_regex = /\b(and|of|the|with)\b|[!?.,-_=;:&\(\)\[\]]/
-    # binding.pry
-    result = input.downcase
-    result = result.gsub(remove_regex, '').squeeze(" ")
+    QuestCom::Handler.prepare_input(input)
   end
 
   def hit_this_url(url)
@@ -27,32 +24,7 @@ class QuestCom::Scraper
     parsedArray = Array(eval(result_body))
     names = parsedArray[1] # example => ["Imp-Binding Contract (Item)", "A Binding Contract (Quest)"]
     potential_matches = names.select {|name| name.include? "(Quest)"}
-
-    if potential_matches.length == 1
-      find_my_id = names.index("#{potential_matches[0]}").to_i
-      quest_id = parsedArray[7][find_my_id][1]
-      quest_id
-    else
-      puts "There is no match. Please try your query on wowhead.com" # temporary message
-      # flesh this out a bit - specific text for no match vs too many matches?
-      # what if the quest HAS NO COMMENTS as well
-    end
-  end
-
-  def tidy_for_json(javascript)
-    # remove key/values that do not matter for the purpose of this program
-    javascript.gsub!(/\b(replies|lastEdit):(\[.*?\])/, '')
-    javascript.gsub!(/(\bcommentv2|number|id|nreplies|sticky|indent|roles|deleted|outofdate):.*?,/, '')
-    javascript.gsub!(/\b,userRating:\d+,+/, '')
-
-    # locate unescaped double quotes to properly escape
-    javascript.gsub!(/(?<!\\)(?:\\{2})*\K"/, '\"')
-    # the user, body and date values are surrounded with single quotes that need to be double
-    user_body_date_regex = /\b(?<key>user|body|date):(?<startQuote>')(?<value>(?:[^']|(?<=\\)')+)(?<endQuote>')/
-    javascript.gsub!(user_body_date_regex, '\k<key>:"\k<value>"')
-    # find each key and surround it with double quotes to make json parser happy
-    javascript.gsub!(/(?<=[{,])([\w]+):/, '"\1":')
-    javascript
+    QuestCom::Handler.analyze_matches(parsedArray, names, potential_matches)
   end
 
   def find_comments_on_quest_page(quest_id)
@@ -62,7 +34,7 @@ class QuestCom::Scraper
     match = /var\s+lv_comments0\s+=\s+(\[.+\]);/.match(result_body)
     # the info at index 1 of the match has all the comment data by itself as javascript
     javascript = match[1]
-    parsable_json = tidy_for_json(javascript)
+    parsable_json = QuestCom::Handler.tidy_for_json(javascript)
     comment_hash_array = JSON.parse(parsable_json) # this gives an array of hashes in tidy JSON
   end
 
