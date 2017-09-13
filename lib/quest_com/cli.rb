dabab790af7a37ff75bea8fe9d2d699e1286b2b7
Meredith Reece <nerdybug@gmail.com>
2017-09-13 22:42:26 +0000

    changesto scraper.new

--- lib/quest_com/cli.rb
+++ lib/quest_com/cli.rb
@@ -4,7 +4,7 @@ class QuestCom::CLI
     QuestCom::Handler.greet_user
     input = Readline.readline
     # binding.pry
-    quest = QuestCom::Scraper.new(input).scrape_to_create_quest_object
+    quest = QuestCom::Scraper.new.from_input_to_quest(input)
     quest.show_top_comment
     # quest.initial_menu
   end
--- lib/quest_com/scraper.rb
+++ lib/quest_com/scraper.rb
@@ -1,8 +1,8 @@
 class QuestCom::Scraper
 
-  def initialize(user_input)
-    @user_input = user_input
-  end
+  # def initialize(user_input)
+  #   @user_input = user_input
+  # end
 
   def hit_this_url(url)
     request = Net::HTTP::Get.new(url.to_s)
@@ -34,13 +34,13 @@ class QuestCom::Scraper
     comment_hash_array = JSON.parse(parsable_json) # this gives an array of hashes in tidy JSON
   end
 
-  def scrape_to_create_quest_object
+  def from_input_to_quest(input)
     # do the work to get from the user's input to creating a QuestData object
-    prepared_input = QuestCom::Handler.prepare_input(@user_input)
+    prepared_input = QuestCom::Handler.prepare_input(input)
     quest_id = parse_quest_id(search_for_result_body(prepared_input))
     sleep 3
     comment_hash_array = find_comments_on_quest_page(quest_id)
-    QuestCom::QuestData.new(quest_id, comment_hash_array)
+    QuestCom::QuestData.new(comment_hash_array)
   end
 
 end
