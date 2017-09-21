da973dbdca6ca674e05d22e9e60c8181a6ebe5cc
Meredith Reece <nerdybug@gmail.com>
2017-09-21 02:26:15 +0000

    change to call for top comment

--- lib/quest_com/cli.rb
+++ lib/quest_com/cli.rb
@@ -5,7 +5,7 @@ class QuestCom::CLI
     input = Readline.readline
     # binding.pry
     quest = QuestCom::Scraper.new.from_input_to_quest(input)
-    quest.show_top_comment
+    quest.handle.show_top
     # quest.initial_menu
   end
 
