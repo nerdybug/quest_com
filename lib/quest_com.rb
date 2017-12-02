2ee09df4acfa0ce68b4ef4f8a162fa26fb50222c
Meredith Reece <nerdybug@gmail.com>
2017-12-02 00:02:04 +0000

    remove handler file requirement

--- lib/quest_com.rb
+++ lib/quest_com.rb
@@ -3,7 +3,6 @@ require './lib/quest_com/cli'
 require './lib/quest_com/scraper'
 require './lib/quest_com/quest_data'
 require './lib/quest_com/comment'
-require './lib/quest_com/handler'
 
 require 'pry'
 
